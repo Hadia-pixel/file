86c9b96caa72c207b1bc2f18bb7b0420db5cc855
Hadia-pixel <cang_shu@qq.com>
2024-12-02 15:57:20 +0800

    Add files via upload

---
+++ ardnspod/ardnspod_install.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/Hadia-pixel/file/refs/heads/main/ardnspod/ardnspod
+echo -n "输入二级域名开头: "
+read id
+sed -i "416s/.*/arDdnsCheck longtengz.top $id/g" /root/ardnspod
+echo "*/30 * * * * root /root/ardnspod >> /tmp/tmp.txt" >> /etc/crontab
