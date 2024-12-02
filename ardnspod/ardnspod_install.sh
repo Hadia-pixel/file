b508f89f707dde5b93ba098229f7b6120b8c51ea
Hadia-pixel <cang_shu@qq.com>
2024-12-02 16:01:59 +0800

    Update ardnspod_install.sh

--- ardnspod/ardnspod_install.sh
+++ ardnspod/ardnspod_install.sh
@@ -1,4 +1,5 @@
 wget https://raw.githubusercontent.com/Hadia-pixel/file/refs/heads/main/ardnspod/ardnspod
+chmod 777 ardnspod
 echo -n "输入二级域名开头: "
 read id
 sed -i "416s/.*/arDdnsCheck longtengz.top $id/g" /root/ardnspod
